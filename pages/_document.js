86f34152e69cd892e7285ddc477c76ba863d7a0f
Tatiana Veratti <Tatiana.Veratti@topbox.io>
2020-03-03 22:44:12 -0600

    add hitsteps

--- pages/_document.js
+++ pages/_document.js
@@ -3,17 +3,21 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
 import theme from '../src/theme';
 
+const hitsteps = `<script type="text/javascript">(function(){var hstc=document.createElement('script'); hstc.src='https://log.hitsteps.com/track.php?code=35678bc2881ac2ea2a09e72f77cca75e';hstc.async=true;var htssc = document.getElementsByTagName('script')[0];htssc.parentNode.insertBefore(hstc, htssc);})();
+</script><noscript><a href="http://www.hitsteps.com/"><img src="//log.hitsteps.com/track.php?mode=img&amp;code=35678bc2881ac2ea2a09e72f77cca75e" alt="web stats" width="1" height="1" />visitor activity monitoring</a></noscript>`;
+
 export default class MyDocument extends Document {
   render() {
     return (
-      <html lang="en" style={{ overflowY: 'scroll' }}>
+      <html lang="en" style={{ overflowY: 'scroll', height: '100%' }}>
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
         </Head>
-        <body>
+        <body style={{ minHeight: '100%' }}>
           <Main />
           <NextScript />
+          <script dangerouslySetInnerHTML={{ __html: hitsteps }} />
         </body>
       </html>
     );
